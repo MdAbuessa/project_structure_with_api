e4a1ae58f36b799554a04cd4238fa2995f7b0391
abuessaflutterdev@gmail.com <abuessaflutterdev@gmail.com>
2025-07-11 09:33:30 +0600

    rx datr details added

--- lib/features/welcome/data/rx_get_wel/rx.dart
+++ lib/features/welcome/data/rx_get_wel/rx.dart
@@ -11,9 +11,15 @@ import '../../../../networks/rx_base.dart';
 
 final class GetWelcomeRX extends RxResponseInt<List<Welcome>> {
   final api = GetWelcomeApi.instance;
+//api নামের একটি ভেরিয়েবল তৈরি হয়েছে, যা GetWelcomeApi
+//ক্লাসের singleton instance।এটি একবারই তৈরি হয়।
 
   GetWelcomeRX({required super.empty, required super.dataFetcher});
+  // কনস্ট্রাক্টরে empty (খালি ডেটা অবস্থা) এবং dataFetcher (স্ট্রিম)
+  // রিসিভ করে, যেগুলো RxResponseInt এর কন্সট্রাক্টরে পাঠানো হয়।
   ValueStream get allWelcomeStream => dataFetcher.stream;
+//এটা Stream রিটার্ন করে, যেখানে Welcome ডেটা আপডেট হলে UI
+// তে রিয়্যাক্টিভভাবে তা দেখা যাবে।
   Future<bool> getWelcome() async {
     try {
       List<Welcome> result = await api.getWelcome();
@@ -23,6 +29,11 @@ final class GetWelcomeRX extends RxResponseInt<List<Welcome>> {
       return handleErrorWithReturn(error);
     }
   }
+
+//getWelcome() একটি Future<bool> ফাংশন,
+//যা API কল করে List<Welcome> ডেটা আনে।
+//সফল হলে handleSuccessWithReturn এ পাঠায়
+//ভুল হলে handleErrorWithReturn চালায়
 
   @override
   handleErrorWithReturn(dynamic error) {
